25dbb459aa234a7df1acebcd4b71bb84cb9f5239
pasta <pasta@dashboost.org>
2021-04-15 20:08:02 -0400

    stuff

--- check_all.py
+++ check_all.py
@@ -1,9 +1,3 @@
-# coding=utf-8
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import os
 import subprocess
 
